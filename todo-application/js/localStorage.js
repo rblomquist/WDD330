a0983a9e63d860c52b917b0e7ef6f0d0bd2c52e9
rblomquist <rblomquist@gmail.com>
2022-10-19 02:39:03 -0400

    continued todo

--- todo-application/js/localStorage.js
+++ todo-application/js/localStorage.js
@@ -1,17 +1,20 @@
-
+// ------------------------------------------
+// object to help work with the local storage
+// ------------------------------------------
 
 class TheLocaleStorage {
     constructor(){
-        // this.toDoList = JSON.parse(localStorage.getItem("test")) || [];
+        // list to help extract information
         this.toDoList = [];
     }
+
     // get item
     getItem(key) {
         const item = localStorage.getItem(key);
         return item;
     }
     // set item 
-    setItem(task, ) {
+    setItem(task, complete) {
         const time = new Date();
         const month = time.getMonth() + 1;
         const day = time.getDate();
@@ -20,61 +23,43 @@ class TheLocaleStorage {
         const timestamp = `${month}-${day}-${year}`;
         const sorting = time.getTime();
 
-        const item = {"timestamp": timestamp, "task": task, "bool": false, "sort": sorting};
+        // saves the item in specific format based on information being stored
+        const item = {"timestamp": timestamp, "task": task, "complete": complete, "sort": sorting};
         this.toDoList.push(item);
 
+        // converts objet to string to be able to be saved in local storage
         const json = JSON.stringify(item);
         localStorage.setItem(task, json);
     }
+    // Remove items
+    removeItems(task) {
+        localStorage.removeItem(task);
+        const updateList = JSON.parse(this.getItem(task))
+        this.toDoList.pop(updateList)
+    }
     // filter items
-    filterItems() {
-        localStorage.filter( (a,b) => a - b );
+    filterItems(bool) {
+                
+        // filters item based on whether it has been completed or not
+        let filteredToDo = this.toDoList.filter( (task) => task["complete"] === bool)
+
+        return filteredToDo
+    }
+
+    refreshList() {
+        this.toDoList = []
+
+        for (let i = 0; i < localStorage.length; i++){
+            let key = localStorage.key(i);
+            // parses the data into an array to extract specific information
+            let item = JSON.parse(this.getItem(key))
+            // stores item in list to be used rather than local storage
+            this.toDoList.push(item)
+        }
+
     }
 }
 
-const testing = new TheLocaleStorage();
-
-testing.setItem("poop");
-// console.log(mara);
-console.log(testing.toDoList)
-console.log(JSON.parse(testing.getItem("poop")))
-console.log(localStorage);
-
-testing.setItem("pee");
-console.log(testing.toDoList)
-
-testing.toDoList = []
-console.log(testing.toDoList)
-for (i = 0; i < localStorage.length; i++){
-    let key = localStorage.key(i);
-    let item = testing.getItem(key)
-    testing.toDoList.push(item)
-}
-console.log(testing.toDoList)
-
-// let myObj = {one: "1", two: "2"}
-// let obj2 = {three: "3", four: "4"}
-
-// console.log(testing.toDoList)
-// console.log(typeof(testing.toDoList))
-
-// testing.setItem("test", myObj);
-// testing.setItem("test", obj2);
-// testing.setItem("test", 'Mara')
-// testing.setItem("apple", 'banana')
-
-// testing.setItem("test", testing.toDoList);
-
-// const mara = JSON.parse(localStorage.getItem("test"));
-
-// console.log(mara)
-// localStorage.clear()
-// console.log(localStorage);
-
-// testing.filterItems()
-
-
-
-// Need Date object for timestamp. 
-
+// exports to be used by event listeners
 export default TheLocaleStorage;
+
