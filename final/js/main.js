79dccbd65c930038cad80651a5e06caf90df351f
rblomquist <rblomquist@gmail.com>
2022-12-05 22:03:32 -0500

    working commit

--- final/js/main.js
+++ final/js/main.js
@@ -1,66 +1,17 @@
 import Views from "./views.js";
 
-// export const pokemon = {}
-// await getOnePokemon();
-// getOnePokemon();
-
-
 const render = new Views;
-window.onload = getAllPokemon(render.homeView);
-// window.onload = getAllPokemon(render.formView);
-
-// let url = "https://pokeapi.co/api/v2/pokemon/";
-// let url1 = "https://pokeapi.co/api/v2/pokemon/151"
+window.onload = render.homeView()
 
 
+document.querySelector("#formBtn").addEventListener("click", () => {
+    document.querySelector("#homeView").classList.add("hidden");
+    render.formView();
+    document.querySelector("#formView").classList.remove("hidden");
+})
 
-// let url = "https://pokeapi.co/api/v2/pokemon?limit=151";
-   
-export async function getAllPokemon(callback) {
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-    
-    if (response.ok) {
-        let data = await response.json();
-        // console.log(data.results)
-        
-        if (callback) {
-        callback()}
-    }  
-}
-
-// async function getAllPokemon() {
-//     return await fetch(url,
-//         {
-//             method: "GET",
-//             headers: {"Accept": "application/json", "Content-Type": "application/json",}
-//         }  )
-//         .then((response) => response.json())
-//     .then((responseData) => {
-//     //   console.log(responseData);
-//       return responseData;
-//     })
-//     .catch(error => console.warn(error));
-// }
-
-// let results = getAllPokemon(url).then(response => {results = response})
-// let results = getAllPokemon(url)
-// console.log(results)
-
-
-async function getOnePokemon() {
-    for (let i=1; i < 152; i++){
-        let url = `https://pokeapi.co/api/v2/pokemon/${i}`
-        let response = await fetch(url);
-
-        if (response.ok) {
-            let data = await response.json();
-            let name = data.name
-            pokemon[name] = data;
-            // console.log(data.name)
-    }}
-    // const storePokemon = JSON.stringify(pokemon);
-    // localStorage.setItem("pokemon", storePokemon)
-}
-
-// console.log(pokemon)
-// console.log(pokemon.length)
+document.querySelector("#backBtn").addEventListener("click", () => {
+    document.querySelector("#formView").classList.add("hidden");
+    render.homeView();
+    document.querySelector("#homeView").classList.remove("hidden");
+})
--- final/js/views.js
+++ final/js/views.js
@@ -1,80 +1,44 @@
-
 export default class Views {
     constructor (){
-        // this.url = "https://pokeapi.co/api/v2/pokemon";
-        // this.offset = 0;
-        // this.code = 2;
-        // this.pokemonList = pokemon
+        this.jsonPokemon = JSON.parse(localStorage.getItem("pokemon"));
     }
 
     homeView() {
-        const pokemon2 = {}
-        const pokemonList = localStorage.getItem("pokemon");
-        const jsonPokemon = JSON.parse(pokemonList)
-        console.log(jsonPokemon)
-        // draws the home page
-        document.body.innerHTML = 
-        `<h1>Pokemon Team</h1>
-        <ul id="mainTeam">
-            <li class="blackBorder active"><h2>${jsonPokemon[1].name}</h2><img><p></p></li>
-            <li class="blackBorder"><h2>${jsonPokemon[2].name}</h2><img><p></p></li>
-            <li class="blackBorder"><h2>${jsonPokemon[3].name}</h2><img><p></p></li>
-            <li class="blackBorder"><h2>${jsonPokemon[4].name}</h2><img><p></p></li>
-            <li class="blackBorder"><h2>${jsonPokemon[5].name}</h2><img><p></p></li>
-            <li class="blackBorder"><h2>${jsonPokemon[6].name}</h2><img><p></p></li>
-        </ul>
-        <div id="mainNav">
-            <button><a href="pokemonList.html">Change</a></button>
-            <button>Interact</button>
-            <button><a href="pokemonList.html">Browse</a></button>
-        </div>`
-
-        // sets up the event listeners for the home page
         const team = document.querySelector("#mainTeam")
         const children = Array.from(team.children)
+        let index = 1
 
+        children.forEach( child => {
+
+            child.querySelector("h2").innerHTML = this.jsonPokemon[index].name
+            getOnePokemon(child, this.jsonPokemon[index].url)
+            index++
+        })
+
+        async function getOnePokemon(child, url) {
+            let response = await fetch(url);
+            
+            if (response.ok) {
+                let data = await response.json();
+                child.querySelector("img").setAttribute("src", data.sprites.front_default)
+                child.querySelector("img").setAttribute("alt", data.name)
+
+                data.stats.forEach(stat => {
+
+                child.querySelector("p").innerHTML += 
+                        `${stat.stat.name} - ${stat.base_stat}<br>`
+                })
+            }
+        }
+        
         children.forEach( child => {child.addEventListener("click", active)})
-
+        
         function active(e) {
             children.forEach( child => {if(child.classList.contains("active")) {
                 child.classList.remove("active")}
             e.currentTarget.classList.add("active")})}
 
-        for (const [key,value] of Object.entries(jsonPokemon)) {
-            // console.log(value.url)
-            getOnePokemon(value.url)
-    
-            
-        }
-
-        async function getOnePokemon(url) {
-            let response = await fetch(url);
-            
-            if (response.ok) {
-                let data = await response.json();
-                console.log(data)
-                // pokemon2[data.name] = data
-                children.forEach (child => {
-                    if(child.querySelector("h2").textContent == data.name) {
-                        console.log(data.name)
-                        child.querySelector("img").setAttribute("src", data.sprites.front_default)
-                        child.querySelector("img").setAttribute("alt", data.name)
-                        // child.querySelector("p").innerHTML = "test"
-
-                        data.stats.forEach(stat => {
-
-                        child.querySelector("p").innerHTML += 
-                                `${stat.stat.name} - ${stat.base_stat}<br>`
-                        })
-
-                    }
-            }  )
-        }
-
-            // jsonPokemon.entries.forEach( ([key, value]) => {console.log(value.url)})
-
     }
-}
 
     formView() {
         async function getAllPokemon() {
@@ -82,43 +46,30 @@ export default class Views {
             
             if (response.ok) {
                 let data = await response.json();
+
+                let list = JSON.parse(localStorage.getItem("pokemon"));
                 
-                document.body.innerHTML =
-                `<h1>Update Team</h1>
-                <form>
-                    <ul id="mainTeam">
-                    </ul>
-                    
-                    <button id="submitBtn">Submit</button>
-                    <input type="submit" id="backBtn" value="back">
-                </form>`
-                
-                buildForm()
-                buildForm()
-                buildForm()
-                buildForm()
-                buildForm()
-                buildForm()
+                for (const [key,value] of Object.entries(list)) {
+                    buildForm(value.name)
+                }
                 submit()
-                back()
 
+                function buildForm(name) {
 
-                function buildForm() {
                     let li = document.createElement("li");
                     let form = document.createElement("form");
                     form.classList.add("blackBorder");
                     
                     let select = document.createElement("select");
                     select.classList.add("selectPokemon")
-                    select.innerHTML = "<option value='test'>Choose Your Pokemon</option>"
+                    select.innerHTML = `<option>${name}</option>`
                     data.results.forEach (result => {select.innerHTML += `<option name=${result.name} value=${result.name}>${result.name}</option>`})
                                         
                     form.appendChild(select);
                     li.appendChild(form);
-                    document.querySelector("#mainTeam").appendChild(form);
+                    document.querySelector("#updateForm").appendChild(form);
                 }
              
-            
                 function submit() {
                     const submitBtn = document.querySelector("#submitBtn");
                     const list = document.querySelectorAll(".selectPokemon")
@@ -137,27 +88,15 @@ export default class Views {
                                 currentList[i] = {"name":item.value, "url": url};
                                 i++ 
                              } );
-                            console.log(currentList);
                             const string = JSON.stringify(currentList);
-                            console.log(string);
                             localStorage.setItem("pokemon", string);
-                            console.log(localStorage);
-                        })
                         
-                }
-
-                function back() {
-                    const backBtn = document.querySelector("#backBtn");
-
-                    // backBtn.addEventListener("click", homeView);
+                            location.reload()
+                        })
                 }
             }
-
         } // Ends getAllPokemon()
 
         getAllPokemon();
-        // console.log(jsonPokemon)
     }
 }
-
-
